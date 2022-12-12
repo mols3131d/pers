edb06062792c8ab3f045016b5d5cab139c246bc5
Sankamita3131 <sankamita3131@gmail.com>
2022-12-13 00:34:52 +0900

    c
    c

--- package/gitig_auth.py
+++
@@ -1,5 +0,0 @@
-auth_key_dict = {
-    "dart": "76dd702fe016b819f3e728f9a63b3b13eadaaeb4",
-    "finaStatInfo": "b8m/px7AI1NzkWN/lcRj9uXgO56otbz7ZI\
-    BfXcplZtC7kmPv1qdNbWD9koAjSQp6t+eo2JJPcn+FiGNuklo98A==",
-}
