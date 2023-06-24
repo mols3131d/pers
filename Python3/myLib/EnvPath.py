7711e2315ba891db72e1eeaaeaff8bc3f532cca9
mols <114456099+mols3131d@users.noreply.github.com>
2023-06-24 19:04:17 +0900

    update EnvPath.py

--- Python3/myLib/EnvPath.py
+++ Python3/myLib/EnvPath.py
@@ -4,25 +4,29 @@ import os
 
 
 class envPath:
-    def __init__(self):
+    def __init__(self, root_path=None, env=None):
         self.os = platform.uname().system
 
-        if self.os == "Linux":
-            # Google_Colab
-            if "google.colab" in sys.modules:
-                from google.colab import drive
+        if env == None:
+            if self.os == "Linux":
+                # Google_Colab
+                if "google.colab" in sys.modules:
+                    from google.colab import drive
 
-                drive.mount("/gdrive", force_remount=True)
-                self.env = "Google_Colab"
-                self.root = "../gdrive/MyDrive"
+                    drive.mount("/gdrive", force_remount=True)
+                    self.env = "Google_Colab"
+                    if root_path == None:
+                        self.root = "../gdrive/MyDrive"
 
-            # Kaggle_notebook
-            elif list(os.walk("/kaggle/"))[0][1] == ["lib", "input", "working"]:
-                self.env = "Kaggle_notebook"
-                self.root = "./kaggle/input"
-        else:
-            self.env = self.os
-            self.root = "./"
+                # Kaggle_notebook
+                elif list(os.walk("/kaggle/"))[0][1] == ["lib", "input", "working"]:
+                    self.env = "Kaggle_notebook"
+                    if root_path == None:
+                        self.root = "./kaggle/input"
+            else:
+                self.env = self.os
+                if root_path == None:
+                    self.root = "."
 
     def __str__(self) -> str:
         return str(platform.uname())
