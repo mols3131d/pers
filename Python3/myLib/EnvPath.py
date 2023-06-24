12c4b61bf9f4e210092d9e3481127169e1090b2e
mols <114456099+mols3131d@users.noreply.github.com>
2023-06-24 17:42:18 +0900

    update EnvPath.py

--- Python3/myLib/EnvPath.py
+++ Python3/myLib/EnvPath.py
@@ -1,64 +1,28 @@
-def envPath(dict_path=None):
-    import platform
-    from pprint import pprint
-
-    global root_path
-    global data_path
-
-    dict_path_default = {
-        "Windows": {"root_path": ".", "data_path": "data"},
-        "Google_Colab": {"root_path": "../gdrive/MyDrive", "data_path": "data"},
-        "Kaggle_notebook": {"root_path": "..", "data_path": "input"},
-    }
-    # 추후에 dict_path에 Kaggle_notebook이 없으면 추가하는 코드 추가 요망
-    if dict_path != None:
-        for k, v in dict_path.items():
-            dict_path_default[k] = v
-
-    dict_path = dict_path_default
+import platform
+import sys
+import os
 
 
-    env = platform.uname().system
+class envPath:
+    def __init__(self):
+        self.os = platform.uname().system
 
-    if env == "Windows":
-        env = env
+        if self.os == "Linux":
+            # Google_Colab
+            if "google.colab" in sys.modules:
+                from google.colab import drive
 
-    elif env == "Linux":
-        import sys
+                drive.mount("/gdrive", force_remount=True)
+                self.env = "Google_Colab"
+                self.root = "../gdrive/MyDrive"
 
-        # Google_Colab
-        if "google.colab" in sys.modules:
-            env = "Google_Colab"
-            from google.colab import drive
-
-            drive.mount("/gdrive", force_remount=True)
-
-        # Kaggle_notebook
+            # Kaggle_notebook
+            elif list(os.walk("/kaggle/"))[0][1] == ["lib", "input", "working"]:
+                self.env = "Kaggle_notebook"
+                self.root = "./kaggle/input"
         else:
-            import os
+            self.env = self.os
+            self.root = "./"
 
-            if list(os.walk("/kaggle/"))[0][1] == ["lib", "input", "working"]:
-                env = "Kaggle_notebook"
-
-    root_path = dict_path[env]["root_path"]
-    data_path = f"{root_path}/{dict_path[env]['data_path']}"
-    print("▣ env :", env)
-    print()
-    print("▣ platform.uname()", "\n", platform.uname(), sep="")
-    print()
-    print("▣ data_path", "\n", data_path, sep="")
-
-
-# path ---- ---- ---- ----
-envPath()
-# train_file = "train.csv"
-# test_file = "test.csv"
-
-# train_path = f"{data_path}/{train_file}"
-# test_path = f"{data_path}/{test_file}"
-# print()
-# print("▣ train_path")
-# print(train_path)
-# print()
-# print("▣ test_path")
-# print(test_path)
+    def __str__(self) -> str:
+        return str(platform.uname())
