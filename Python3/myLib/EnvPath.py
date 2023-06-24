3894a32a87336d56cd4478244cb0f4dae4582172
mols <114456099+mols3131d@users.noreply.github.com>
2023-06-25 00:26:16 +0900

    update cote.ipynb and EnvPath.py

--- Python3/myLib/EnvPath.py
+++ Python3/myLib/EnvPath.py
@@ -4,29 +4,37 @@ import os
 
 
 class envPath:
-    def __init__(self, root_path=None, env=None):
+    def __init__(self):
+        # os
         self.os = platform.uname().system
 
-        if env == None:
-            if self.os == "Linux":
-                # Google_Colab
-                if "google.colab" in sys.modules:
-                    from google.colab import drive
+        # env
+        if self.os == "Linux":
+            # Google_Colab
+            if "google.colab" in sys.modules:
+                from google.colab import drive
 
-                    drive.mount("/gdrive", force_remount=True)
-                    self.env = "Google_Colab"
-                    if root_path == None:
-                        self.root = "../gdrive/MyDrive"
+                drive.mount("/content/drive", force_remount=True)
+                self.env = "Google_Colab"
+            # Kaggle_notebook
+            elif list(os.walk("/kaggle/"))[0][1] == [
+                "lib",
+                "input",
+                "working",
+            ]:
+                self.env = "Kaggle_notebook"
+        else:
+            self.env = self.os
 
-                # Kaggle_notebook
-                elif list(os.walk("/kaggle/"))[0][1] == ["lib", "input", "working"]:
-                    self.env = "Kaggle_notebook"
-                    if root_path == None:
-                        self.root = "./kaggle/input"
-            else:
-                self.env = self.os
-                if root_path == None:
-                    self.root = "."
+        # root
+        self.root = os.getcwd()
+
+        if self.env == "Google_Colab":
+            self.root = "../content"
+            self.root = "../content/drive/MyDrive"
+
+        # data
+        self.data = f"{self.root}/data"
 
     def __str__(self) -> str:
         return str(platform.uname())
