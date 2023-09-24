c1e40ed21c4555cb709f9fa74659404bbbea8ec6
mols <114456099+mols3131d@users.noreply.github.com>
2023-09-24 21:43:09 +0900

    update 38 files and delete 3 files

---
+++ Coding Test with JavaScript/input_lnx.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const inp = fs.readFileSync("/dev/stdin").toString();
+
+consol.log(inp);
---
+++ Coding Test with JavaScript/input_win.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const inp = fs.readFileSync("input.txt").toString();
+
+consol.log(inp);
